b823513b4612b154917b2a9c84c2aeb764f7558b
watermelon-bytes <water@melons.io>
2026-04-08 14:39:24 +0300

    create `types.h` to provide convinient type aliases

---
+++ FreezeProject/include/types.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+typedef unsigned int uint;
+typedef uint16_t u16;
+typedef int16_t i16;
+typedef uint32_t u32;
+typedef int32_t i32;
+typedef uint8_t u8;
+typedef int8_t i8;
+typedef uintptr_t register_t;
